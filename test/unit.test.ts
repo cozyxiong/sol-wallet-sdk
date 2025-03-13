c6b11e598cf2f63b53ebc92008667ad966306d11
cozyxiong <cozyxiong@163.com>
2025-03-13 20:10:15 +0800

    fix bug

--- test/unit.test.ts
+++ test/unit.test.ts
@@ -10,7 +10,7 @@ describe("sol wallet test", () => {
     })
 
     test("import sol wallet", () => {
-        const secretKey = "aaad9398934126d3f4c6c530806a16da69122057d7ad67d3f31f9fd38db014ef6ad9f14590589c71f4fc7b393b8dee8d8d39c7da9da3be9cd658f461c57afcf7";
+        const secretKey = "";
         const addressInfo = importSolWallet(secretKey);
         console.log(addressInfo);
     })
